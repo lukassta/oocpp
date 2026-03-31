e9482e85dbb0515675bfb6e6d8e9f7312b65aa17
Lukas Stavaris <lukas.stavaris@gmail.com>
2026-03-31 11:17:08 +0300

    Init basic tree234 class and headers

---
+++ ip2/tree234.cpp
@@ -0,0 +1,8 @@
+namespace Tree234Namespace {
+    class Tree234 {
+        Tree234() {
+        }
+        ~Tree234() {
+        }
+    };
+}
---
+++ ip2/tree234.h
@@ -0,0 +1,6 @@
+#ifndef TREE234_H
+#define TREE234_H
+
+class Tree234;
+
+#endif
